425041e9b023b90b9b16c6ad017a86fa8a0b30f0
William <77687139+williamcouto@users.noreply.github.com>
2024-02-28 18:48:19 -0300

    Update script.js
    remove name

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function CalculaIMC(){
-    let name = document.getElementById('nome_usuario').value
     let peso = document.getElementById('peso_usuario').value
     let altura = document.getElementById('altura_usuario').value
     let resultado = document.querySelector('div#resultado')
     let valorIMC = (peso / (altura * altura)).toFixed(2)
 
     //Verifica se os campos estão vazios.
-    if(name.length === '' || peso.length <= 0 || altura.length === ''){
+    if(peso.length <= 0 || altura.length === ''){
         window.alert("ERRO: dados incompletos!")
     }
+    //Verifica os pesos.
     if(valorIMC < 18.5){
-        resultado.innerHTML = `Olá, ${name}! você está abaixo do peso.`
+        resultado.innerHTML = `Você está abaixo do peso.`
     }
     else if((valorIMC >= 18.5) && (valorIMC < 25)){
-        resultado.innerHTML = `Olá, ${name}! você está com o peso normal.`
+        resultado.innerHTML = `Você está com o peso normal.`
     }
     else if((valorIMC >= 25) && (valorIMC < 30)){
-        resultado.innerHTML = `Olá, ${name}! você está sobrepeso.`
+        resultado.innerHTML = `Você está sobrepeso.`
     }
     else if((valorIMC >= 30) && (valorIMC < 35)){
-        resultado.innerHTML = `Olá, ${name}! você está com obesidade.`
+        resultado.innerHTML = `Você está com obesidade.`
     }
     else if((valorIMC >= 35) && (valorIMC < 40)){
-        resultado.innerHTML = `Olá ${name}  CUIDADO! você está com obesidade severa.`
+        resultado.innerHTML = `CUIDADO! você está com obesidade severa.`
     }   
     else if(valorIMC > 40){
-        resultado.innerHTML = `Olá, ${name} CUIDADO! você está com obesidade mórbida.`
-    }
+        resultado.innerHTML = `CUIDADO! você está com obesidade mórbida.`
+    }  
 }
