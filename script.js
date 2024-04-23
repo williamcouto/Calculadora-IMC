68d275236bdea7371b47e32da80d1bb3a63b3812
William <77687139+williamcouto@users.noreply.github.com>
2024-04-22 21:48:31 -0300

    Update script.js
    preenchendo a div de status com uma cor referente a cada IMC.

--- script.js
+++ script.js
@@ -2,29 +2,45 @@ function CalculaIMC(){
     let peso = document.getElementById('peso_usuario').value
     let altura = document.getElementById('altura_usuario').value
     let resultado = document.querySelector('div#resultado')
+    let resultIMC = document.getElementById('result_IMC')
     let valorIMC = (peso / (altura * altura)).toFixed(2)
-
     //Verifica se os campos estão vazios.
     if(peso.length <= 0 || altura.length === ''){
         window.alert("ERRO: dados incompletos!")
     }
-    //Verifica os pesos.
+    
     if(valorIMC < 18.5){
-        resultado.innerHTML = `Você está abaixo do peso.`
+        resultIMC.style.backgroundColor = 'blue'
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#C9EEFF'
+        resultado.innerHTML = `Abaixo do peso`
     }
     else if((valorIMC >= 18.5) && (valorIMC < 25)){
-        resultado.innerHTML = `Você está com o peso normal.`
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#4FFF52'
+        resultado.innerHTML = `Peso normal`
     }
     else if((valorIMC >= 25) && (valorIMC < 30)){
-        resultado.innerHTML = `Você está sobrepeso.`
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#FEE020'
+        resultado.innerHTML = `Sobrepeso`
     }
     else if((valorIMC >= 30) && (valorIMC < 35)){
-        resultado.innerHTML = `Você está com obesidade.`
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#FF0A1F'
+        resultIMC.style.color = "#FFFF"
+        resultado.innerHTML = `Obesidade`
     }
     else if((valorIMC >= 35) && (valorIMC < 40)){
-        resultado.innerHTML = `CUIDADO! você está com obesidade severa.`
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#CC0011'
+        resultIMC.style.color = "#FFFF"
+        resultado.innerHTML = `Obesidade severa`
     }   
     else if(valorIMC > 40){
-        resultado.innerHTML = `CUIDADO! você está com obesidade mórbida.`
-    }  
+        resultIMC.innerHTML = `${valorIMC}`
+        resultIMC.style.backgroundColor = '#660008'
+        resultIMC.style.color = "#FFFF"
+        resultado.innerHTML = `Obesidade mórbida`
+    }
 }
